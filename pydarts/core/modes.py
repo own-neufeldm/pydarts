c1ea0bf902b94241a7b532d7a44a34f17fd48986
Martin Neufeld <m.neufeld946@gmail.com>
2024-08-13 23:12:26 +0200

    feat(core/modes): add options and 701 mode

--- pydarts/core/modes.py
+++ pydarts/core/modes.py
@@ -11,6 +11,10 @@ class BaseMode():
         raise NotImplementedError()
 
     @classmethod
+    def get_options(cls) -> list[str]:
+        raise NotImplementedError()
+
+    @classmethod
     def get_initial_score(cls) -> int:
         raise NotImplementedError()
 
@@ -29,6 +33,13 @@ class Mode301(BaseMode):
         return (
             "All players start with 301 points, the goal is to reach 0."
         )
+
+    @classmethod
+    def get_options(cls) -> list[str]:
+        return [
+            "Double in",
+            "Double out",
+        ]
 
     @classmethod
     def get_initial_score(cls) -> int:
@@ -51,5 +62,39 @@ class Mode501(BaseMode):
         )
 
     @classmethod
+    def get_options(cls) -> list[str]:
+        return [
+            "Double in",
+            "Double out",
+        ]
+
+    @classmethod
     def get_initial_score(cls) -> int:
         return 501
+
+
+class Mode701(BaseMode):
+    def __init__(self) -> None:
+        super().__init__()
+        return None
+
+    @classmethod
+    def get_name(cls) -> str:
+        return "701"
+
+    @classmethod
+    def get_description(cls) -> str:
+        return (
+            "All players start with 701 points, the goal is to reach 0."
+        )
+
+    @classmethod
+    def get_options(cls) -> list[str]:
+        return [
+            "Double in",
+            "Double out",
+        ]
+
+    @classmethod
+    def get_initial_score(cls) -> int:
+        return 701
